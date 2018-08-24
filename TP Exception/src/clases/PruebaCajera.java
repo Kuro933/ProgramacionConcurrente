c4bbcf93966b6462412b7e1e38156ca391f1d054
Federico <33671243+Kuro933@users.noreply.github.com>
2018-08-24 16:39:10 -0300

    Add files via upload

--- TP Exception/src/clases/PruebaCajera.java
+++ TP Exception/src/clases/PruebaCajera.java
@@ -3,16 +3,22 @@ package clases;
 public class PruebaCajera {
 
 	public static void main(String[] args) {
+		long initialTime = System.currentTimeMillis();
 		
 		Cliente cliente1 = new Cliente("Cliente 1" , new int[] {2,2,1,5,2,3});
 		Cliente cliente2 = new Cliente("Cliente 2" , new int[] {1,3,5,1,1});
-		Cajera cajera1 = new Cajera("Cajera1" , 25);
 		
 		//Tiempo inicial de referencia
 		
-		long initialTime = System.currentTimeMillis();
-		cajera1.procesarCompra(cliente1, initialTime);
-		cajera1.procesarCompra(cliente2, initialTime);
+		CajeraIncisoB cajera1 = new CajeraIncisoB("Cajera1" , 25 , cliente1 ,initialTime);
+		CajeraIncisoB cajera2 = new CajeraIncisoB("Cajera2" , 27 , cliente2 , initialTime);
+		
+		Thread t1 = new Thread(cajera1);
+		Thread t2 = new Thread(cajera2);
+		
+		
+		t1.start();
+		t2.start();
 		
 	}
 	
