006add8d9e194e2c497af5303470b7cac5787095
Federico <33671243+Kuro933@users.noreply.github.com>
2018-08-23 22:56:26 -0300

    Add files via upload

---
+++ TP Exception/src/clases/Cliente.java
@@ -0,0 +1,38 @@
+package clases;
+
+public class Cliente {
+
+	private String nombre;
+	private int[] carrito;
+	
+	public Cliente(String nomb, int[] arr) {
+		this.nombre = nomb;
+		this.carrito = arr;
+	}
+	 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int[] getCarrito() {
+		return carrito;
+	}
+
+	public void setCarrito(int[] carrito) {
+		this.carrito = carrito;
+	}
+	
+	public String toString() {
+		String s="";
+		
+		for(int j=0; j<carrito.length;j++) {
+			s += carrito[j] + " ";
+		}
+		
+		return s;
+	}
+}
---
+++ TP Exception/src/clases/PruebaCajera.java
@@ -0,0 +1,19 @@
+package clases;
+
+public class PruebaCajera {
+
+	public static void main(String[] args) {
+		
+		Cliente cliente1 = new Cliente("Cliente 1" , new int[] {2,2,1,5,2,3});
+		Cliente cliente2 = new Cliente("Cliente 2" , new int[] {1,3,5,1,1});
+		Cajera cajera1 = new Cajera("Cajera1" , 25);
+		
+		//Tiempo inicial de referencia
+		
+		long initialTime = System.currentTimeMillis();
+		cajera1.procesarCompra(cliente1, initialTime);
+		cajera1.procesarCompra(cliente2, initialTime);
+		
+	}
+	
+}
